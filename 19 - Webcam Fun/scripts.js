56fb74ffe22eb85fd15d7158860079063a8bb4d5
neotamoe <neota.moe@gmail.com>
2019-09-07 18:36:16 -0500

    initial steps

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,19 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function takePhoto(){
+    console.log("TAKE PHOTO!")
+}
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+        video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+        console.log("Something went wrong!");
+    });
+}
+

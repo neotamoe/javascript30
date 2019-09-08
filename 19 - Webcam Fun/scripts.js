d22d3f92b1875a7cec13c37ad366594491e6e927
neotamoe <neota.moe@gmail.com>
2019-09-07 19:37:33 -0500

    image saving to strip

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -4,9 +4,20 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
-
+let photoRoll = [];
 function takePhoto(){
-    console.log("TAKE PHOTO!")
+    console.log("TAKE PHOTO!");
+    const snapped = ctx.drawImage(video, 0,0, canvas.width, canvas.height);
+    var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+
+    photoRoll.push(image)
+    console.log(photoRoll)
+
+    strip.innerHTML =
+    photoRoll
+      .map(photo => `<img src="${photo}" />`)
+      .join('');
+
 }
 
 if (navigator.mediaDevices.getUserMedia) {
